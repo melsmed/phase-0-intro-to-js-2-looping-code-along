3676e2ef46977947941773ec004cd31d4bd2924a
Melissa Medina <melissa.med52@gmail.com>
2023-05-25 13:35:35 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let messages = []
+
+    for (let i=0; i < names.length; i++) {
+        let message = (`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+        messages.push(message);
+    }
+
+    return messages
+}
+let names = ["Guadalupe", "Ollie", "Aki"]
+let eventName = "surprise"
+
+let messages = writeCards(names, eventName)
+
+console.log(messages)
+
+
+
+
+
+
+
+function countDown (number) {
+    while (number >=0) {
+        console.log (number)
+        number--
+
+    }
+}
